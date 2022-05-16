9595386b2d5c6fdbd453f4ff43fce7b00c9111f9
Ayush Mehta <31724299+ayushm1998@users.noreply.github.com>
2022-05-16 18:04:54 +0530

    Update index.js

--- resourceGraph/index.js
+++ resourceGraph/index.js
@@ -3,10 +3,6 @@ const resourceGraph = require("@azure/arm-resourcegraph");
 const {ClientSecretCredential} = require("@azure/identity");
 const fs = require("fs");
 require("dotenv").config();
-const {BlobServiceClient} = require('@azure/storage-blob');
-const blobConnectionString= 'DefaultEndpointsProtocol=https;AccountName=icertispocaed1;AccountKey=Cz17vRyqBiNM2hJ5oIw5L+uLYdA0QXl2qPNJOz/9LhWLU4QVp0T8a81dAvv+zGkpa4skmpt1WMJ5+AStcbB6Qw==;EndpointSuffix=core.windows.net'
-const blobContainerClient= 'recommendation-container'
-const getCredentials = require('../../helpers/getCredentials')
 
 
 const query = async () => {
@@ -14,10 +10,7 @@ const query = async () => {
 
 
    let cred = await getCredentials(csmartID).catch(err => {reject(err)})
-    // const tenantId = "e4e34038-ea1f-4882-b6e8-ccd776459ca0";
-    // const clientId = "e1456d9f-8f5d-4b56-8d3a-7d36c9c7d28a";
-    // const clientSecret = "kxN7Q~UEpu~zqp9K8r8rHLKKX.99f0se9pEQH";
-    
+  
     credentials = new ClientSecretCredential(cred[0].tenantId, cred[0].clientId, cred[0].clientSecret);
     console.log(credentials)
     const client = new resourceGraph.ResourceGraphClient(credentials);
